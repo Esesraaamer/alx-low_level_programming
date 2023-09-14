13a36a7786db012d7096b1c1ad46acc56144e605
Esesraaamer <esraaamer52@gmail.com>
2023-09-14 15:31:35 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
+ * main - causes an infinite loop
+ * Return: 0
 */
 
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
 	/*while (i < 10)*/
 	/*{*/
 
-	/*	putchar(i);*/
+	/*    putchar(i);*/
 
 	/*}*/
 
